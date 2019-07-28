2617fe50d4ba83780665205dfe5fe746c198a944
qinjialei <954551222@qq.com>
2019-07-28 19:15:02 +0800

    1

--- diff算法/patch.js
+++ diff算法/patch.js
@@ -1,10 +1,27 @@
-function patch(oldTree, newTree) {
-  patchAttr(oldTree, oldTree.attr, newTree.attr)
+import { each } from "./utils.js";
+function patch(oldTreeEl, oldTree, newTree) {
+  patchAttr(oldTreeEl, oldTree.attr, newTree.attr)
 
 }
 
-function patchAttr(oldTree, oldTreeAttr, newTreeAttr) {
+function patchAttr(oldTreeEl, oldTreeAttr = {}, newTreeAttr = {}) {
+  each(oldTreeAttr, (key, value => {//遍历  oldTreeAttr 看newTreeAttr 是否还有对应的属性
+    if (newTreeAttr[key]) {
+      value !== newTreeAttr[key] && oldTreeEl.setAttribute(key, newTreeAttr[key])
+    }
+    else {
+      oldTreeEl.removeAttribute(key)
+    }
+  }))
 
+  each(newTreeAttr, (key, value => {//看 oldTreeAttr 是否还有对应的属性，没有就新增 有就修改或者不做操作
+    if (newTreeAttr[key]) {
+      value !== newTreeAttr[key] && oldTreeEl.setAttribute(key, newTreeAttr[key])
+    }
+    else {
+      oldTreeEl.removeAttribute(key)
+    }
+  }))
 
 }
 
